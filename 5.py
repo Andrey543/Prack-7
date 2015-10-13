aa1cfb135cd2fc79b4b9a2149504fa1a80d2de1d
Andrey543 <andrey_rybakov_97@mail.ru>
2015-10-13 11:13:50 +0300

    Упражнение

---
+++ 5.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import math
+r=input()
+
+
+
+def f(x):
+    return eval(r)
+_x=[x/1000 for x in range(-10000,10000)]
+y=[f(x) for x in _x]
+plt.plot(_x,y)
+plt.xkcd()
+plt.show()
