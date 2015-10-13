e5a2972e8f6a1cb9e535e85bdaca54be56559164
Andrey543 <andrey_rybakov_97@mail.ru>
2015-10-13 10:33:16 +0300

    Упрражнение №2

--- 2.py
+++ 2.py
@@ -1,8 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
+def y(x):
+    return x*x-6-x
 x=np.arange(-20,20,0.01)
-plt.plot(x,x*x-6-x)
+plt.plot(x,y(x))
+plt.grid(True)
 plt.show()
 
+c=0.0000000001
+a=-10
+b=10
+o=20
+
+while o>c:
+    k=(a+b)/2
+    if (y(k+c)>y(k-c)):
+        if (y(k)>0):
+            b=k
+        else:
+            a=k
+    else:
+        a=k
+    o=b-a
+print((a+b)/2)
 
 
+a=-10
+b=10
+o=20
+while o>c:
+    k=(a+b)/2
+    if (y(k+c)<y(k-c)):
+        if (y(k)>0):
+            a=k
+        else:
+            b=k
+    else:
+        b=k
+    o=b-a
+print((a+b)/2)
+    
