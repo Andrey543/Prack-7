bda52c9b8c5586151d2b7fcdb63d4b46721c3279
Andrey543 <andrey_rybakov_97@mail.ru>
2015-10-13 09:40:28 +0300

    Упражнение №1

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 import math
 def y(x):
-    return math.log((1/math.e**math.sin(x)+1)/(5/4+1/x**15),1+x**2)
+    return math.log((1/math.e**(math.sin(x)+1))/(5/4+1/x**15),1+x**2)
 print(y(1))
 print(y(10))
 print(y(1000))
